743c9dce506e1115cf154de902fe4726302d575b
AlbertoAlfredo <raioalfredo@gmail.com>
2021-04-21 20:29:55 -0300

    base home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import './Header.css';
+import { Button } from '@material-ui/core';
 import {
     // BrowserRouter as Router,
     // HashRouter,
@@ -6,22 +7,49 @@ import {
     // Route,
     Link
 } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        // light: será calculada com base em palette.primary.main,
+        main: '#ff4400',
+        // dark: será calculada com base em palette.primary.main,
+        // contrastText: será calculada para contrastar com palette.primary.main
+      },
+      secondary: {
+        light: '#0066ff',
+        main: '#0044ff',
+        // dark: será calculada com base palette.secondary.main,
+        contrastText: '#ffcc00',
+      },
+      // Usado por `getContrastText()` para maximizar o contraste entre
+      // o plano de fundo e o texto.
+      contrastThreshold: 3,
+      // Usado pelas funções abaixo para mudança de uma cor de luminância por aproximadamente
+      // dois índices dentro de sua paleta tonal.
+      // Por exemplo, mude de Red 500 para Red 300 ou Red 700.
+      tonalOffset: 0.2,
+    },
+  });
 
 function Header() {
     return (
         <header>
+        <ThemeProvider theme={theme}>
         <section class="banner">
             <img class="banner-img" src="/jardimdeefesto/ms-icon-310x310.png" />
         </section>
         <nav class="navbar">
             <ul class="navbar-list">
                 <li>
-                    <Link to="/home">Home</Link>
+                    <Link to="/home"><Button color="primary">Home</Button></Link>
                 </li><li>
-                    <Link to="/agora">Ágora</Link>
+                    <Link to="/agora"><Button color="primary">Ágora</Button></Link>
                 </li>
             </ul>
         </nav>
+        </ThemeProvider>
         </header>
     );
 };
