d1c82495b9e0f54985b2728205cb93ee0e249521
AlbertoAlfredo <raioalfredo@gmail.com>
2021-06-01 16:58:36 -0300

    encurtador

--- src/components/data/barracas.js
+++ src/components/data/barracas.js
@@ -1,7 +1,8 @@
 
     var barracas =
     [
-    {"id": 1, "title": 'Elemental', "content": 'Jóias e Variedades', "img": "elemental.jpg", "url": "https://lojaelemental.com.br?Ref=20"},
+    {"id": 1, "title": 'Elemental', "content": 'Jóias e Variedades', "img": "elemental.jpg", "url": "encurtador.com.br/fhlLO"},
+    //{"id": 1, "title": 'Elemental', "content": 'Jóias e Variedades', "img": "elemental.jpg", "url": "https://lojaelemental.com.br?Ref=20"},
     // {"id": 2, "title": "Titulo Teste2", "content": "Welcome to the jungle baby2", "img": "produtoTeste.jpg", "url": "teste"},
     // {"id": 3, "title": "Titulo Teste3", "content": "Welcome to the jungle baby3", "img": "produtoTeste.jpg", "url": "teste"},
     // {"id": 4, "title": "Titulo Teste4", "content": "Welcome to the jungle baby4", "img": "produtoTeste.jpg", "url": "teste"},
