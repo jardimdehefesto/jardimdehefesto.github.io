4a33f3ad56a73841592ed0ea3e510f62c0404136
AlbertoAlfredo <raioalfredo@gmail.com>
2021-06-02 03:36:05 -0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "02cf72dca47ed8e3ebbc"; }
+/******/ 		__webpack_require__.h = function() { return "8fc79e5d993d238df56a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- _next/static/webpack/webpack.9a002d99ccff41043615.hot-update.js
+++ _next/static/webpack/webpack.9a002d99ccff41043615.hot-update.js
@@ -12,7 +12,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "231cd730245227433f23"; }
+/******/ 	__webpack_require__.h = function() { return "8e19881bc138ebef919e"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
