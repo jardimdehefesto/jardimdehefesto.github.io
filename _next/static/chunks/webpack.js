475eadd03b3f556b9823daae20c1307f01e8a2f2
AlbertoAlfredo <raioalfredo@gmail.com>
2021-06-04 17:26:29 -0300

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6d02a1d8eee28d0248aa"; }
+/******/ 		__webpack_require__.h = function() { return "b664e6a2c5a2dd973444"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
